8507c4a2497210340fe531f8906451bc9f6bfafe
Asriel <fr2500@protonmail.com>
2023-01-07 19:42:35 +0300

    Removed unsused usings

--- src/Shatus.YouTube/YoutubeDownloader.cs
+++ src/Shatus.YouTube/YoutubeDownloader.cs
@@ -1,20 +1,19 @@
-﻿using System.Net.Http;
-using YoutubeExplode;
+﻿using YoutubeExplode;
 using YoutubeExplode.Converter;
 
 namespace Shatus.YouTube;
 
 public class YoutubeDownloader : IVideoDownloader
 {
-	private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IHttpClientFactory _httpClientFactory;
 
-	public YoutubeDownloader(IHttpClientFactory httpClientFactory)
-	{
-		_httpClientFactory = httpClientFactory;
-	}
-	public async Task DownloadVideoAsync(string link, string outputFolder, string fileName)
-	{
-		using var httpClient = _httpClientFactory.CreateClient();
+    public YoutubeDownloader(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task DownloadVideoAsync(string link, string outputFolder, string fileName)
+    {
+        using var httpClient = _httpClientFactory.CreateClient();
         var youtubeClient = new YoutubeClient(httpClient);
         await youtubeClient.Videos.DownloadAsync(link, $"{outputFolder}\\{fileName}");
     }
